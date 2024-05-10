7f8a32de34ad482cb21b3727299626e490b1fcdb
Abhinav Kapur <abhinav.kapur1408@gmail.com>
2024-05-09 19:08:51 -0700

    Updates

---
+++ precache-manifest.d55dea7dd424ccbf28742e8339d8934c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a8fd8911767854af79b976bd9fb716",
+    "url": "/ecommerce-react-js/index.html"
+  },
+  {
+    "revision": "6d60cf9fd971b301da55",
+    "url": "/ecommerce-react-js/static/css/main.0d616ac5.chunk.css"
+  },
+  {
+    "revision": "0f2e559d6c6b91281c05",
+    "url": "/ecommerce-react-js/static/js/2.8dc51f34.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/ecommerce-react-js/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "6d60cf9fd971b301da55",
+    "url": "/ecommerce-react-js/static/js/main.1b24e541.chunk.js"
+  },
+  {
+    "revision": "86c651714d481f9cdf04",
+    "url": "/ecommerce-react-js/static/js/runtime~main.06f829e4.js"
+  },
+  {
+    "revision": "0a88fe12d24aefbf3b783d5b605d954f",
+    "url": "/ecommerce-react-js/static/media/1.0a88fe12.png"
+  },
+  {
+    "revision": "10df74551ec92f71a6c0c301a01072d5",
+    "url": "/ecommerce-react-js/static/media/2.10df7455.png"
+  },
+  {
+    "revision": "549596dc07591b5bb6af0be538a0863f",
+    "url": "/ecommerce-react-js/static/media/3.549596dc.png"
+  },
+  {
+    "revision": "2589290cd47d276b6a65fa8619881e0c",
+    "url": "/ecommerce-react-js/static/media/4.2589290c.png"
+  },
+  {
+    "revision": "0a8b1a897fdb58ba42e7a8c45e76a1be",
+    "url": "/ecommerce-react-js/static/media/5.0a8b1a89.png"
+  },
+  {
+    "revision": "36e80c8db9c91636d7b88dbd44145056",
+    "url": "/ecommerce-react-js/static/media/6.36e80c8d.webp"
+  },
+  {
+    "revision": "cabd9079d01f42a40c2310d929396bca",
+    "url": "/ecommerce-react-js/static/media/7.cabd9079.webp"
+  },
+  {
+    "revision": "07235760d2ea665be2e8b686bb6a3791",
+    "url": "/ecommerce-react-js/static/media/8.07235760.webp"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ecommerce-react-js/static/media/logo.06e73328.svg"
+  }
+]);
